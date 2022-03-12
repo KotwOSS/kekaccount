fbc51ff8be1c8409c7b289ea7089e8c0778a44d7
KekOnTheWorld <kekontheworld@gmail.com>
2022-03-12 12:31:30 +0100

    Fixed state system

--- src/api/http.rs
+++ src/api/http.rs
@@ -1,4 +1,5 @@
 use std::io::Error;
+use std::sync::Arc;
 
 use actix_web::{HttpServer, App, web};
 
@@ -7,22 +8,25 @@ use crate::colors;
 use crate::routes;
 
 pub struct State {
-    pub pool: PgPool
+    pub pool: Arc<PgPool>
 }
 
-pub async fn main(pool: PgPool, address: String, port: u16) -> Result<(), Error> {
+pub async fn main(pool: Arc<PgPool>, address: String, port: u16) -> Result<(), Error> {
     println!("{}START{} http on {}{}:{}", colors::LIGHT_BLUE, colors::RESET, colors::ORANGE,  address, port);
-
     match HttpServer::new(move || {
         App::new()
             .service(routes::api::index)
             .service(routes::api::auth::token::create::create)
+            .service(routes::api::auth::register::register)
             .app_data(web::Data::new(State { pool: pool.clone() }))
     })
     .bind((address, port))?
     .run()
     .await {
-        Ok(v) => Ok(v),
+        Ok(v) => {
+            println!("{}STOP{} http", colors::ORANGE, colors::RESET);
+            Ok(v)
+        },
         Err(error) => {
             println!("{}ERROR{} while binding http: {}", colors::RED, colors::RESET, error);
             return Err(error);
