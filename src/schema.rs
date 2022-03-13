72ec101b921cb900ccdbf24c4b3d34af92e45dad
KekOnTheWorld <kekontheworld@gmail.com>
2022-03-13 18:36:19 +0100

    Added app table

--- src/schema.rs
+++ src/schema.rs
@@ -1,6 +1,18 @@
 table! {
+    apps (id) {
+        id -> Bytea,
+        owner -> Bytea,
+        name -> Varchar,
+        description -> Varchar,
+        redirect_uri -> Varchar,
+        homepage -> Varchar,
+    }
+}
+
+table! {
     tokens (id) {
         id -> Bytea,
+        token -> Bytea,
         user_id -> Bytea,
         name -> Varchar,
         permissions -> Int2,
@@ -17,6 +29,7 @@ table! {
 }
 
 allow_tables_to_appear_in_same_query!(
+    apps,
     tokens,
     users,
 );
