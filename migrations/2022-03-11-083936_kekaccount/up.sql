60612f3fb189e1cf030fbdb758ad164595620825
KekOnTheWorld <kekontheworld@gmail.com>
2022-03-13 18:39:04 +0100

    Added app table

--- migrations/2022-03-11-083936_kekaccount/up.sql
+++ migrations/2022-03-11-083936_kekaccount/up.sql
@@ -8,7 +8,17 @@ CREATE TABLE users (
 
 CREATE TABLE tokens (
     id BYTEA PRIMARY KEY,
+    token BYTEA NOT NULL,
     user_id BYTEA NOT NULL,
     name VARCHAR(32) NOT NULL,
     permissions SMALLINT NOT NULL
+);
+
+CREATE TABLE apps (
+    id BYTEA PRIMARY KEY,
+    owner BYTEA NOT NULL,
+    name VARCHAR(32) NOT NULL,
+    description VARCHAR(255) NOT NULL,
+    redirect_uri VARCHAR(255) NOT NULL,
+    homepage VARCHAR(255) NOT NULL
 );
