9ee45ac933d5133a0bb51967a0c5741c9e08eb7b
KekOnTheWorld <kekontheworld@gmail.com>
2022-03-13 18:33:29 +0100

    Changed String to &'a str for more flexibility while parsing hex

--- src/util/hex.rs
+++ src/util/hex.rs
@@ -11,7 +11,7 @@ impl Display for HexError {
 }
 
 
-pub fn parse_to_buf(hex_string: String, expected_len: usize) -> Result<Vec<u8>, HexError> {
+pub fn parse_to_buf<'a>(hex_string: &'a str, expected_len: usize) -> Result<Vec<u8>, HexError> {
     if hex_string.len() != expected_len {
         return Err(HexError { msg: format!("Expected length {}, but found {}", expected_len, hex_string.len()) })
     }
