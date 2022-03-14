3a03b14474c455f5f746998cc2af2b33b02654af
KekOnTheWorld <kekontheworld@gmail.com>
2022-03-14 20:36:32 +0100

    Added methods for parsing hex

--- src/util/checker.rs
+++ src/util/checker.rs
@@ -1,8 +1,9 @@
+use actix_web::http::header::HeaderMap;
 use diesel::{QueryResult, PgConnection, r2d2::ConnectionManager};
 use r2d2::PooledConnection;
 use regex::{self, Regex};
 use tokio::sync::OnceCell;
-use crate::{errors::actix::{JsonError, JsonErrorType}, colors, models::{user, token}, database::PgPool};
+use crate::{errors::actix::{JsonError, JsonErrorType}, util, colors, models::{user, token}, database::PgPool};
 
 static EMAIL_REGEX: OnceCell<Regex> = OnceCell::const_new();
 //static URL_REGEX: OnceCell<Regex> = OnceCell::const_new();
@@ -65,18 +66,31 @@ pub fn get_con(pool: &PgPool) -> Result<PooledConnection<ConnectionManager<PgCon
     )))
 }
 
+pub fn hex_header<'a>(name: &'a str, len: usize, headers: &HeaderMap)->Result<Vec<u8>, JsonError> {
+    hex(name, map_opt(headers.get(name), format!("Missing {} header", name).as_str())?.to_str().unwrap(), len)
+}
+
+pub fn hex<'a, 'b>(name: &'a str, hex: &'b str, len: usize)->Result<Vec<u8>, JsonError> {
+    util::hex::parse_to_buf(hex, len)
+        .map_err(|e| JsonErrorType::BAD_REQUEST.new_error(format!(
+            "Error while parsing {}: {}",
+            name,
+            e
+        )))
+}
+
 pub fn authorize(token: Vec<u8>, db_connection: &PgConnection) -> Result<(user::User, token::Token), JsonError> {
     let tokens = map_qres(token::Token::find_token(token, db_connection), "Error while selecting tokens")?;
 
-    match tokens.first() {
+    match tokens.into_iter().next() {
         Some(token) => {
-            let users = map_qres(user::User::find(token.clone_user_id(), db_connection), "Error while selecting users")?;
+            let users = map_qres(user::User::find(token.user_id.clone(), db_connection), "Error while selecting users")?;
 
-            match users.first() {
+            match users.into_iter().next() {
                 None=>Err(JsonErrorType::NOT_FOUND.new_error(format!(
                     "The account behind this token doesn't exist anymore. Has it been deleted?"
                 ))),
-                Some(user)=>Ok((user.clone(), token.clone()))
+                Some(user)=>Ok((user, token))
             }
         },
         None => Err(JsonErrorType::BAD_CREDENTIALS.new_error(format!(
