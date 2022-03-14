dffb52a8b961367264e7b3635e749419618caa84
KekOnTheWorld <kekontheworld@gmail.com>
2022-03-14 20:52:05 +0100

    Fixed expected hex length and added error when nothing got deleted

--- src/routes/api/apps/delete.rs
+++ src/routes/api/apps/delete.rs
@@ -15,7 +15,7 @@ pub struct DeleteData {
 pub async fn delete(delete_data: web::Json<DeleteData>, state: web::Data<State>, request: HttpRequest) -> Result<impl Responder> {
     let token = hex_header("Authorization", 256, request.headers())?;
 
-    let id = checker::hex("id", delete_data.id.as_str(), 20)?;
+    let id = checker::hex("id", delete_data.id.as_str(), 40)?;
 
     let db_connection = &checker::get_con(&state.pool)?;
 
@@ -27,10 +27,14 @@ pub async fn delete(delete_data: web::Json<DeleteData>, state: web::Data<State>,
             token.permissions
         )).into());
     } else {
-        map_qres(app::App::delete_owner(id, user.id, db_connection), "Error while deleting app")?;
-
-        return Ok(web::Json(json!({
-            "success": true
-        })));
+        return match map_qres(app::App::delete_owner(id, user.id, db_connection), "Error while deleting app")? {
+            0 => Err(JsonErrorType::NOT_FOUND.new_error(format!(
+                "App with id '{}' not found!",
+                delete_data.id
+            )).into()),
+            _ => Ok(web::Json(json!({
+                "success": true
+            })))
+        };
     }
 }
