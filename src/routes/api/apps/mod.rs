51c0bf67085d18df01d92c1aff1391af39fedc4f
KekOnTheWorld <kekontheworld@gmail.com>
2022-03-15 21:13:33 +0100

    Added app retrieve route

--- src/routes/api/apps/mod.rs
+++ src/routes/api/apps/mod.rs
@@ -3,3 +3,4 @@ pub mod delete;
 pub mod list;
 pub mod update;
 pub mod search;
+pub mod retrieve;
---
+++ src/routes/api/apps/retrieve.rs
@@ -0,0 +1,32 @@
+use hex::ToHex;
+
+use actix_web::{web, Result, Responder, get};
+
+use crate::errors::actix::JsonErrorType;
+use crate::models::app;
+use crate::api::http::State;
+use crate::util::checker::{self, map_qres};
+
+#[get("/api/apps/{id}")]
+pub async fn retrieve(path: web::Path<(String,)>, state: web::Data<State>) -> Result<impl Responder> {
+    let id_hex = path.into_inner().0;
+    let id = checker::hex("id", id_hex.as_str(), 40)?;
+
+    let db_connection = &checker::get_con(&state.pool)?;
+
+    let apps = map_qres(app::App::find(id, db_connection), "Error while selecting apps")?;
+
+    match apps.into_iter().next() {
+        Some(app)=>Ok(web::Json(json!({
+            "name": app.name,
+            "owner": app.owner.encode_hex::<String>(),
+            "homepage": app.homepage,
+            "description": app.description,
+            "id": id_hex
+        }))),
+        None=>Err(JsonErrorType::NOT_FOUND.new_error(format!(
+            "App with id '{}' not found!",
+            id_hex
+        )).into())
+    }
+}
