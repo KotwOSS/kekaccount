5c115be3cf1c091132e2c15efa4775dc85dbb373
KekOnTheWorld <kekontheworld@gmail.com>
2022-03-15 17:47:55 +0100

    Added methods for updating apps

--- src/models/app.rs
+++ src/models/app.rs
@@ -13,6 +13,17 @@ pub struct App {
     pub homepage: String,
 }
 
+#[derive(AsChangeset)]
+#[table_name="apps"]
+pub struct AppChangeSet {
+    pub id: Option<Vec<u8>>,
+    pub owner: Option<Vec<u8>>,
+    pub name: Option<String>,
+    pub description: Option<String>,
+    pub redirect_uri: Option<String>,
+    pub homepage: Option<String>,
+}
+
 impl App {
     pub fn create(&self, connection: &PgConnection) -> QueryResult<usize> {
         diesel::insert_into(apps::table)
@@ -43,6 +54,13 @@ impl App {
     pub fn delete_owner(id: Vec<u8>, owner: Vec<u8>, connection: &PgConnection) -> QueryResult<usize> {
         diesel::delete(apps::table)
             .filter(apps::dsl::id.eq(id).and(apps::dsl::owner.eq(owner)))
+            .execute(connection)
+    }
+
+    pub fn update_owner(id: Vec<u8>, owner: Vec<u8>, changes: &AppChangeSet, connection: &PgConnection) -> QueryResult<usize> {
+        diesel::update(apps::table)
+            .filter(apps::dsl::id.eq(id).and(apps::dsl::owner.eq(owner)))
+            .set(changes)
             .execute(connection)
     }
 }
