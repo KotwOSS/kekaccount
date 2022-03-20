77ecba0a536fbc09c4cb69f195d226047fb96ab6
KekOnTheWorld <kekontheworld@gmail.com>
2022-03-20 19:35:05 +0100

    Added email sent route

---
+++ frontend/src/routes/email_sent.svelte
@@ -0,0 +1,43 @@
+<script>
+    import { goto } from '$app/navigation';
+
+    function redirect_login(e) {
+        e.preventDefault();
+        goto("/login");
+    }
+</script>
+
+<main class="root">
+    <form class="inner">
+        <h1>Verification email sent</h1>
+        <button on:click={redirect_login}>Login</button>
+    </form>
+</main>
+
+
+<style>
+    @import url("../themes/default.css");
+
+    main {
+        width: 100%;
+        height: 100%;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+
+    h1 {
+        margin-bottom: 20px;
+    }
+
+    form {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        width: 100%;
+        max-width: 450px;
+        padding: 30px 10px;
+        
+    }
+</style>
