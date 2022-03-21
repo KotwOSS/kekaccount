5cebd1bbe1496bfa46654d476fee97389b72a5f2
KekOnTheWorld <kekontheworld@gmail.com>
2022-03-21 20:08:36 +0100

    Added redirect to /login or /dash

---
+++ frontend/src/routes/index.svelte
@@ -0,0 +1,23 @@
+<script>
+    import { goto } from "$app/navigation";
+    import { is_authorized } from "../api";
+    import { onMount } from "svelte";
+
+    onMount(async ()=>{
+        if(await is_authorized()) goto("/dash");
+        else goto("/login");
+    });
+</script>
+
+<div class="root">
+
+</div>
+
+<style>
+    @import url("../themes/default.css");
+
+    .root {
+        height: 100%;
+        width: 100%;
+    }
+</style>
