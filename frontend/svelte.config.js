42c754662cc850e37466208a3cec5f70de4f0a3d
KekOnTheWorld <kekontheworld@gmail.com>
2022-03-26 14:20:42 +0100

    Runned format

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -1,5 +1,5 @@
-import adapter from '@sveltejs/adapter-static';
-import preprocess from 'svelte-preprocess';
+import adapter from "@sveltejs/adapter-static";
+import preprocess from "svelte-preprocess";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -9,11 +9,11 @@ const config = {
 
 	kit: {
 		adapter: adapter(),
-        prerender: {
-            default: true
-        },
-        trailingSlash: "always"
-	},
+		prerender: {
+			default: true
+		},
+		trailingSlash: "always"
+	}
 };
 
 export default config;
