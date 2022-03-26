147f4bae13d28360406fc6c3c3ff4e0ff8a51eaa
KekOnTheWorld <kekontheworld@gmail.com>
2022-03-26 14:08:36 +0100

    Added login actions

--- frontend/src/routes/login.svelte
+++ frontend/src/routes/login.svelte
@@ -1,201 +1,243 @@
 <script lang="ts">
-    import { goto } from '$app/navigation';
-    import Loader from '../components/loader.svelte';
-    import { onMount } from "svelte";
-    import { Routes, hash_password, get_identifier, APIError } from "../api";
+	import { goto } from "$app/navigation";
+	import Loader from "../components/loader.svelte";
+	import actions, { type Action } from "../actions";
+	import { Routes, hash_password, get_identifier, APIError } from "../api";
+	import { onMount } from "svelte";
 
-    let uoe;
-    let password;
+	let uoe;
+	let password;
 
-    let username: string;
-    let creds: boolean = false;
+	let username: string;
 
-    let error: undefined|string;
+	let error: undefined | string;
 
-    let success: boolean;
+	let action: Action;
 
-    let token: string;
-    let valid: boolean = false;
-    let user: any;
+	let success: boolean;
 
-    let submit_loading: boolean = true;
+	let token: string;
+	let valid: boolean = false;
+	let user: any;
 
-    let params: URLSearchParams;
-    let redirect: string;
+	let submit_loading: boolean = true;
 
-    let force: boolean = false;
+	let params: URLSearchParams;
+	let redirect: string;
 
-    onMount(async ()=>{
-        params = new URLSearchParams(window.location.search);
-        redirect = params.get("r")??"/dash";
+	let force: boolean = false;
 
-        username = localStorage.getItem("uoe");
-        creds = localStorage.getItem("creds")!==null;
+	onMount(async () => {
+		params = new URLSearchParams(window.location.search);
+		redirect = params.get("r") ?? "/dash";
 
-        force = params.get("force")==="true";
+		username = localStorage.getItem("uoe");
 
-        token = localStorage.getItem("token");
-        if(token) {
-            try {
-                let info = await Routes.Auth.Token.INFO.send({token});
-                console.log(info);
-                if(info.token.perms === -1) {
-                    valid = true;
-                    user = await Routes.User.INFO.send({token});
-                }
-            } catch(e: any) {}
-        }
+		action = actions[params.get("a")];
 
-        submit_loading = false;
-    });
+		force = params.get("force") === "true";
 
-    async function login(e) {
-        submit_loading = true;
-        e.preventDefault();
- 
-        let hashed_password = hash_password(password.value);
-        let identifier = get_identifier(uoe.value, hashed_password);
+		token = localStorage.getItem("token");
+		if (token) {
+			try {
+				let info = await Routes.Auth.Token.INFO.send({ token });
+				console.log(info);
+				if (info.token.perms === -1) {
+					valid = true;
+					user = await Routes.User.INFO.send({ token });
+				}
+			} catch (e: any) {}
+		}
 
-        success = true;
-        if(!valid || force) await regenerate_token(identifier);
+		submit_loading = false;
+	});
 
-        if(success) goto(redirect);
-        else submit_loading = false;
-    }
+	async function login(e) {
+		e.preventDefault();
 
-    async function regenerate_token(identifier: any) {
-        console.log("regenerate " + valid);
-        localStorage.removeItem("token");
-        try {
-            let response = await Routes.Auth.Token.CREATE.send({
-                name: "login", permissions: -1, identifier
-            });
-            localStorage.setItem("token", response.token);
-            success = true;
-        } catch(e: any) {
-            success = false;
-            if(e instanceof APIError) {
-                error = e.get_message();
-            } else error = "Connetion issues";
-        }
-    }
+		submit_loading = true;
 
-    function confirm_access() {
-        let hashed_password = hash_password(password.value);
-        localStorage.setItem("uoe", uoe.value);
-        localStorage.setItem("password", hashed_password);
+		let hashed_password = hash_password(password.value);
+		let identifier = get_identifier(uoe.value, hashed_password);
 
-        localStorage.removeItem("creds");
+		success = true;
+		if (!valid || force) await regenerate_token(identifier);
 
-        goto(redirect);
-    }
+		if (success) goto(redirect);
+		else submit_loading = false;
+	}
+
+	async function regenerate_token(identifier: any) {
+		console.log("regenerate " + valid);
+		localStorage.removeItem("token");
+		try {
+			let response = await Routes.Auth.Token.CREATE.send({
+				name: "login",
+				permissions: -1,
+				identifier
+			});
+			localStorage.setItem("token", response.token);
+			success = true;
+		} catch (e: any) {
+			success = false;
+			if (e instanceof APIError) {
+				error = e.get_message();
+			} else error = "Connetion issues";
+		}
+	}
+
+	async function confirm_access(e) {
+		e.preventDefault();
+
+		submit_loading = true;
+
+		let hashed_password = hash_password(password.value);
+
+		let identifier = get_identifier(uoe.value, hashed_password);
+		try {
+			await Routes.Auth.INFO.send({ identifier });
+
+			localStorage.setItem("uoe", uoe.value);
+			localStorage.setItem("password", hashed_password);
+
+			goto(action.url);
+		} catch (e) {
+			submit_loading = false;
+			if (e instanceof APIError) {
+				error = e.get_message();
+			} else error = "Connetion issues";
+		}
+	}
 </script>
 
 <div class="root">
-    <form on:submit={login} class="inner">
-        <main class="blend-in">
-            {#if creds}
-            <h1>Confirm access</h1>
-            <input bind:this={uoe} value={username??""} disabled={username!==null} type="text" placeholder="username or email">
-            <input bind:this={password} type="password" placeholder="password">
-            <button on:click={confirm_access}>Confirm</button>
-            {:else}
-            <h1>Login</h1>
-            <input bind:this={uoe} type="text" placeholder="username or email">
-            <input bind:this={password} type="password" placeholder="password">
-            {#if error}
-                <p class="error break">{error}</p>
-            {/if}
-            <p class="register">Don't have an account? <a href="/register">Register</a></p>
-            {#if user && !force}
-            <p class="hint">There already exists a valid token for the user {user.name}! A new token wont be generated! <a href="/logout">Logout</a></p>
-            {/if}
-            <button class:active={submit_loading} disabled={submit_loading}>
-            {#if submit_loading}
-                <Loader />
-            {:else}
-                Login
-            {/if}
-            </button>
-            {/if}
-        </main>
-    </form>    
+	<form on:submit={login} class="inner">
+		<main class="blend-in" class:confirm={action}>
+			{#if action}
+				<h1>Confirm access</h1>
+				{#if action.warning}<h3 class="warning">{action.warning}</h3>{/if}
+				{#if action.hint}<h3 class="hint">{action.hint}</h3>{/if}
+				<input
+					bind:this={uoe}
+					value={username ?? ""}
+					disabled={username !== null}
+					type="text"
+					placeholder="username or email"
+				/>
+				<input bind:this={password} type="password" placeholder="password" />
+				{#if error}
+					<p class="error break">{error}</p>
+				{/if}
+				<button on:click={confirm_access} class:active={submit_loading} disabled={submit_loading}>
+					{#if submit_loading}
+						<Loader />
+					{:else}
+						Confirm
+					{/if}
+				</button>
+			{:else}
+				<h1>Login</h1>
+				<input bind:this={uoe} type="text" placeholder="username or email" />
+				<input bind:this={password} type="password" placeholder="password" />
+				{#if error}
+					<p class="error break">{error}</p>
+				{/if}
+				<p class="register">Don't have an account? <a href="/register">Register</a></p>
+				{#if user && !force}
+					<p class="hint">
+						There already exists a valid token for the user {user.name}! A new token wont be
+						generated! <a href="/logout">Logout</a> <a href="/dash">Dashboard</a>
+					</p>
+				{/if}
+				<button class:active={submit_loading} disabled={submit_loading}>
+					{#if submit_loading}
+						<Loader />
+					{:else}
+						Login
+					{/if}
+				</button>
+			{/if}
+		</main>
+	</form>
 </div>
 
 <style>
-    @import url("../themes/default.css");
+	@import url("../themes/default.css");
 
-    .register {
-        margin-top: 5px;
-        margin-bottom: 5px;
-    }
+	.register {
+		margin-top: 5px;
+		margin-bottom: 5px;
+	}
 
-    .root :global(#loading) {
-        width: 22px;
-        height: 22px;
-    }
+	.root :global(#loading) {
+		width: 22px;
+		height: 22px;
+	}
 
-    .root {
-        width: 100%;
-        height: 100%;
-        display: flex;
-        align-items: center;
-        justify-content: center;
-    }
+	.root {
+		width: 100%;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+	}
 
-    h1 {
-        margin-bottom: 10px;
-    }
+	h1 {
+		margin-bottom: 10px;
+	}
 
-    input {
-        margin-bottom: 5px;
-        text-align: center;
-    }
+	input {
+		margin-bottom: 5px;
+		text-align: center;
+	}
 
-    button {
-        margin-top: 10px;
-    }
+	button {
+		margin-top: 10px;
+	}
 
-    form {
-        overflow: hidden;
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        justify-content: center;
-        width: 100%;
-        max-width: 450px;
-        padding: 30px 10px;
-        
-    }
+	form {
+		overflow: hidden;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+		width: 100%;
+		max-width: 450px;
+		padding: 30px 10px;
+	}
 
-    main {
-        overflow: hidden;
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        justify-content: center;
-        width: 100%;
-    }
+	main {
+		overflow: hidden;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+		width: 100%;
+	}
 
-    @keyframes error-blend-in {
-        0% { 
-            opacity: 0;
-            height: 0;
-        }
-        100% { 
-            opacity: 1;
-            height: 20px;
-            margin-top: 8px;
-            margin-bottom: 5px;
-        }
-    }
+	@keyframes error-blend-in {
+		0% {
+			opacity: 0;
+			height: 0;
+		}
+		100% {
+			opacity: 1;
+			height: 20px;
+			margin-top: 8px;
+			margin-bottom: 5px;
+		}
+	}
 
-    .error {
-        animation: error-blend-in 0.3s ease forwards;
-        text-align: center;
-    }
+	.error {
+		animation: error-blend-in 0.3s ease forwards;
+		text-align: center;
+	}
 
-    .hint {
-        margin-bottom: 5px;
-    }
+	.hint {
+		margin-bottom: 5px;
+	}
+
+	.confirm > .warning,
+	.confirm > .hint {
+		margin-bottom: 15px;
+	}
 </style>
