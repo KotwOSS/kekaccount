e07086a2a5b2fd462bd2143f8eac883db1b87234
KekOnTheWorld <kekontheworld@gmail.com>
2022-03-26 11:38:47 +0100

    Added documentation config

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,2 +1,3 @@
 export const api_base = "http://localhost:5070/api";
-export const default_avatar = "https://u.gamepowerx.com/d/boTKXj_"
+export const default_avatar = "https://u.gamepowerx.com/d/boTKXj"
+export const perm_docs = "https://oss.kotw.dev/kekaccount/docs/PERMISSIONS"
