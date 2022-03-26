d6c7883fbae2d699c8240421e3e4e7e707cf65c4
KekOnTheWorld <kekontheworld@gmail.com>
2022-03-26 14:09:39 +0100

    Changed to using login actions

--- frontend/src/routes/verify.svelte
+++ frontend/src/routes/verify.svelte
@@ -1,60 +1,53 @@
 <script>
-    import { goto } from "$app/navigation";
-    import { APIError, get_identifier, get_login_redirect, Routes } from "../api";
-    import { onMount } from "svelte";
+	import { goto } from "$app/navigation";
+	import { APIError, get_identifier, get_identifier_ls, get_login_redirect, Routes } from "../api";
+	import { onMount } from "svelte";
 
-    let error;
+	let error;
 
-    onMount(()=>{
-        let params = new URLSearchParams(window.location.search);
-        let id = params.get("id");
+	onMount(() => {
+		let params = new URLSearchParams(window.location.search);
+		let id = params.get("id") ?? localStorage.getItem("verify");
+		localStorage.setItem("verify", id);
 
-        let uoe = localStorage.getItem("uoe");
-        localStorage.removeItem("uoe");
-        let password = localStorage.getItem("password");
-        localStorage.removeItem("password");
-
-        if(id) {
-            if(uoe && password) {
-                let identifier = get_identifier(uoe, password);
-                Routes.Auth.VERIFY.send({
-                    id,
-                    identifier
-                })
-                .then(r=>goto("/dash"))
-                .catch(e=>{
-                    if(e instanceof APIError) {
-                        console.log(e.get_message());
-                        error = e.get_message();
-                    }
-                });
-            } else {
-                localStorage.setItem("creds", "true");
-                goto(get_login_redirect());
-            }
-        } else goto("/dash");
-    });
+		if (id) {
+			let identifier = get_identifier_ls();
+			if (identifier) {
+				Routes.Auth.VERIFY.send({
+					id,
+					identifier
+				})
+					.then((r) => goto("/dash"))
+					.catch((e) => {
+						if (e instanceof APIError) {
+							console.log(e.get_message());
+							error = e.get_message();
+						}
+					});
+			} else goto("/login?a=2");
+		} else goto("/dash");
+	});
 </script>
 
 {#if error}
-<h2>Error while verifying</h2>
-<p class="error">{error}</p>
-<a href="/dash">Dashboard</a>
+	<h2>Error while verifying</h2>
+	<p class="error">{error}</p>
+	<a href="/dash">Dashboard</a>
 {/if}
 
 <style>
-    @import url("../themes/default.css");
+	@import url("../themes/default.css");
 
-    a {
-        text-decoration: none;
-        color: aqua;
-    }
+	a {
+		text-decoration: none;
+		color: aqua;
+	}
 
-    .error {
-        color: rgb(255, 62, 62); 
-        margin-bottom: 10px;
-        word-wrap: break-word;
-        word-break: break-all;
-        line-break: strict;
-    }
+	.error {
+		color: rgb(255, 62, 62);
+		margin-bottom: 10px;
+		word-wrap: break-word;
+		word-break: break-all;
+		line-break: strict;
+	}
 </style>
