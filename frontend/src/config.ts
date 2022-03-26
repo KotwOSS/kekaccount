0328fdda668aebb37e614b01d796cc018e987f3f
KekOnTheWorld <kekontheworld@gmail.com>
2022-03-26 14:07:48 +0100

    Added hint

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,3 +1,5 @@
-export const api_base = "http://localhost:5070/api";
-export const default_avatar = "https://u.gamepowerx.com/d/boTKXj"
-export const perm_docs = "https://oss.kotw.dev/kekaccount/docs/PERMISSIONS"
+export const api_base = "http://192.168.178.61:5070/api";
+export const default_avatar = "https://u.gamepowerx.com/d/boTKXj_";
+// Falls craftingdragon seine seite down macht
+//export const default_avatar = "https://u.kotw.dev/e/dq0cy3b";
+export const perm_docs = "https://oss.kotw.dev/kekaccount/docs/PERMISSIONS";
