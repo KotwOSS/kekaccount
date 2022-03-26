562c3764778bcced2fd2f4e4414b9e8787c5037d
KekOnTheWorld <kekontheworld@gmail.com>
2022-03-26 14:08:16 +0100

    Refactor and fixed looks

--- frontend/src/routes/email_sent.svelte
+++ frontend/src/routes/email_sent.svelte
@@ -1,48 +1,45 @@
 <script>
-    import { goto } from '$app/navigation';
+	import { goto } from "$app/navigation";
 
-    function redirect_login(e) {
-        e.preventDefault();
-        goto("/login");
-    }
+	function redirect_login(e) {
+		e.preventDefault();
+		goto("/login");
+	}
 </script>
 
 <main class="root">
-    <form class="inner">
-        <h1>Verification email sent</h1>
-        <p>Please check your inbox including the spam folder</p>
-        <button on:click={redirect_login}>Login</button>
-    </form>
+	<form class="inner">
+		<h1>Verification email sent</h1>
+		<p>Please check your inbox including the spam folder</p>
+		<button on:click={redirect_login}>Login</button>
+	</form>
 </main>
 
-
 <style>
-    @import url("../themes/default.css");
+	@import url("../themes/default.css");
 
-    main {
-        width: 100%;
-        height: 100%;
-        display: flex;
-        align-items: center;
-        justify-content: center;
-    }
+	.root {
+		width: 100%;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+	}
 
-    h1 {
-        margin-bottom: 5px;
-    }
+	h1 {
+		margin-bottom: 5px;
+	}
 
-    p {
-        margin-bottom: 15px;
-    }
+	p {
+		margin-bottom: 15px;
+	}
 
-    form {
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        justify-content: center;
-        width: 100%;
-        max-width: 450px;
-        padding: 30px 10px;
-        
-    }
+	form {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+		width: 100%;
+		max-width: 450px;
+		padding: 30px 10px;
+	}
 </style>
