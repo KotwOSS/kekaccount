06b2e098eb6f8275db970ff7e115760c5dbada8d
KekOnTheWorld <kekontheworld@gmail.com>
2022-03-26 11:39:22 +0100

    Added ability to delete tokens

--- frontend/src/routes/dash/tokens.svelte
+++ frontend/src/routes/dash/tokens.svelte
@@ -1,8 +1,9 @@
 <script lang="ts">
+    import { onMount } from "svelte";
     import { goto } from "$app/navigation";
     import Loader from "../../components/loader.svelte";
-    import { APIError, get_login_redirect, Routes } from "../../api";
-    import { onMount } from "svelte";
+    import Perms from "../../components/perms.svelte";
+    import { APIError, get_identifier, get_login_redirect, Routes } from "../../api";
 
     let user: any;
     let loading = true;
@@ -12,7 +13,28 @@
     let tokens_loading: boolean = false;
     let tokens_error: string;
 
+    let show_token: any;
+
     onMount(async ()=>{
+        let delete_token = localStorage.getItem("delete_token");
+        if(delete_token) {
+            localStorage.removeItem("delete_token");
+
+            let uoe = localStorage.getItem("uoe");
+            let password = localStorage.getItem("password");
+            if(uoe && password) {
+                let identifier = get_identifier(uoe, password);
+                try {
+                    await Routes.Auth.Token.DELETE.send({id: delete_token, identifier});
+                } catch(e) {
+                    if(e instanceof APIError) {
+                        tokens_error = e.get_message();
+                    } else tokens_error = "Connection issues";
+                }
+                
+            }
+        }
+
         token = localStorage.getItem("token");
         if(token) {
             try {
@@ -50,7 +72,7 @@
         {:else}
         <main class="blend-in">
             <h1>Tokens</h1>
-            <p>Here you can see and manage all your tokens.</p>
+            <p>Here you can see and manage all your tokens. <a href="/dash">Dashboard</a></p>
 
             {#if !user.verified}
                 <p class="not-verified hint">IMPORTANT: Your email is not yet verified! Non verified users may get deleted.</p>
@@ -59,14 +81,38 @@
             {#if tokens_loading}
             <Loader />
             {:else}
+            {#if show_token}
+            <div class="show_token blend-in border">
+                {#if show_token.active}
+                <p class="hint">This is the token youre currently logged in with</p>
+                {/if}
+
+                <h2>{show_token.name}</h2>
+                <p class="id">{show_token.id}</p>
+                <Perms perms={show_token.perms} />
+
+                <div class="actions">
+                    <button class="delete" on:click={()=>{
+                        localStorage.setItem("creds", "true");
+                        localStorage.setItem("uoe", user.name);
+                        localStorage.setItem("delete_token", show_token.id);
+                        goto(get_login_redirect());
+                    }}>Delete</button>
+                    <button class="back" on:click={()=>show_token = undefined}>Back</button>
+                </div>
+            </div>
+            {:else}
             <div class="tokens blend-in">
                 {#each tokens as tk}
-                    <div class:active={tk.active} class="token border hhigh">
+                    <div on:click={()=>{
+                        show_token = tk;
+                    }} class:active={tk.active} class="token border hhigh">
                         <h2>{tk.name}</h2>
                         <p>{tk.id}</p>
                     </div>
                 {/each}
             </div>
+            {/if}
             {/if}
 
             {#if tokens_error}
@@ -112,6 +158,7 @@
     }
 
     main {
+        width: 100%;
         display: flex;
         align-items: center;
         flex-direction: column;
@@ -139,6 +186,10 @@
         margin: 0 10px;
     }
 
+    .refresh {
+        margin-top: 15px;
+    }
+
     .tokens {
         margin: 10px 0;
         margin-bottom: 10px;
@@ -152,5 +203,32 @@
         cursor: pointer;
         transition: all 0.3s ease;
         margin-top: 8px;
+    }
+
+    .show_token {
+        padding: 10px;
+        transition: all 0.3s ease;
+        margin: 20px 0;
+        width: 98%;
+    }
+
+    .show_token>.id {
+        margin-top: 5px;
+        margin-bottom: 10px;
+    }
+
+    .show_token>.hint {
+        margin-bottom: 10px;
+    }
+
+    .show_token>.actions {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        margin-top: 20px;
+    }
+
+    .show_token>.actions>.delete {
+        margin-right: 10px;
     }
 </style>
