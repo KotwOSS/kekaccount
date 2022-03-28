f1c8329d9e73836948496f8ac8a014208483702e
KekOnTheWorld <kekontheworld@gmail.com>
2022-03-28 20:09:42 +0200

    Added error handler

---
+++ frontend/src/routes/__error.svelte
@@ -0,0 +1,15 @@
+<div class="root">
+    <h1>Probably a 404</h1>
+    <p>To lazy to check tho</p>
+</div>
+
+<style>
+    .root {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        background: linear-gradient(180deg, rgba(0, 0, 0, 0.8) 0%, rgba(0, 0, 0, 1) 50%), url('/bg.jpg');
+        background-size: cover;
+    }
+</style>
