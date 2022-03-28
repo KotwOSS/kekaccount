8a0f73853fa0251c5ceb7cc5d99f5e1b78803420
KekOnTheWorld <kekontheworld@gmail.com>
2022-03-28 20:10:41 +0200

    Redesigned component system

--- frontend/src/checker.ts
+++
@@ -1,4 +0,0 @@
-export const STRONG_PASSWORD = /(?=.*?[#?!@$%^&*-])/;
-export const POOR_PASSWORD = /[a-z]/;
-export const WEAK_PASSWORD = /(?=.*?[0-9])/;
-export const WHITESPACE = /^$|\s+/;
--- frontend/src/config.ts
+++
@@ -1,5 +0,0 @@
-export const api_base = "http://192.168.178.61:5070/api";
-export const default_avatar = "https://u.gamepowerx.com/d/boTKXj_";
-// Falls craftingdragon seine seite down macht
-//export const default_avatar = "https://u.kotw.dev/e/dq0cy3b";
-export const perm_docs = "https://oss.kotw.dev/kekaccount/docs/PERMISSIONS";
