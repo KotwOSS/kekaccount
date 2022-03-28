b1787ef559fe2124ac76a13e46b19ef843d58235
KekOnTheWorld <kekontheworld@gmail.com>
2022-03-28 20:06:40 +0200

    Added footer

---
+++ frontend/src/components/footer.svelte
@@ -0,0 +1,17 @@
+<footer>
+    <p class="copyright">(C) 2022 KekOnTheWorld</p>
+    <div class="event">
+        <a href="https://www.stopputin.net/" target="_blank">🇺🇦 Stop putin</a>
+    </div>
+</footer>
+
+<style>
+    footer {
+        width: 100%;
+        padding: 15px;
+        display: flex;
+        gap: 20px;
+        justify-content: center;
+        align-items: center;
+    }
+</style>
