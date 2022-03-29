fd0b72f52e772ad7796e59e656e85bbef0170123
KekOnTheWorld <kekontheworld@gmail.com>
2022-03-29 19:28:41 +0200

    Added login page

---
+++ frontend/src/routes/login.svelte
@@ -0,0 +1,12 @@
+<div class="root" />
+
+<style>
+	.root {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		background: linear-gradient(180deg, rgba(0, 0, 0, 0.8) 0%, rgba(0, 0, 0, 1) 50%), url("/bg.jpg");
+		background-size: cover;
+	}
+</style>
