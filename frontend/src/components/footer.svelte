8b3b436a7d62d5d0ab7abd4540aee05404e61695
KekOnTheWorld <kekontheworld@gmail.com>
2022-04-01 20:09:40 +0200

    Formatted

--- frontend/src/components/footer.svelte
+++ frontend/src/components/footer.svelte
@@ -1,21 +1,21 @@
 <script lang="ts">
-    let events: any;
-    fetch("https://events.kotw.dev/recent.json")
-        .then(r => r.json())
-        .then(r => events = r);
+	let events: any;
+	fetch("https://events.kotw.dev/recent.json")
+		.then((r) => r.json())
+		.then((r) => (events = r));
 </script>
 
 <footer>
 	<p class="copyright">(C) 2022 KekOnTheWorld</p>
-    {#if events}
-        <div class="events">
-            {#each events as event}
-            <div class="event">
-                <a href={event.link} target="_blank">{event.name}</a>
-            </div>
-            {/each}
-        </div>
-    {/if}
+	{#if events}
+		<div class="events">
+			{#each events as event}
+				<div class="event">
+					<a href={event.link} target="_blank">{event.name}</a>
+				</div>
+			{/each}
+		</div>
+	{/if}
 </footer>
 
 <style>
@@ -23,18 +23,18 @@
 		width: 100%;
 		padding: 15px;
 		display: flex;
-        flex-direction: column;
+		flex-direction: column;
 		gap: 20px;
 		justify-content: center;
 		align-items: center;
 	}
 
-    .events {
-        display: flex;
-        flex-direction: row;
-        justify-content: center;
-        align-items: center;
-        flex-wrap: wrap;
-        gap: 15px;
-    }
+	.events {
+		display: flex;
+		flex-direction: row;
+		justify-content: center;
+		align-items: center;
+		flex-wrap: wrap;
+		gap: 15px;
+	}
 </style>
