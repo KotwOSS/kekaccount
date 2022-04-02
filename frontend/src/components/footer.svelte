2714474880fad9dd2ec7c64b3903ff83d5efb62f
KekOnTheWorld <kekontheworld@gmail.com>
2022-04-02 16:11:13 +0200

    Made footer more professional

--- frontend/src/components/footer.svelte
+++ frontend/src/components/footer.svelte
@@ -1,4 +1,8 @@
 <script lang="ts">
+    import { imprint, privacy } from "$lib/config";
+    import T from "$components/translate.svelte";
+    import { LangKey as lk } from "$lib/lang";
+
 	let events: any;
 	fetch("https://events.kotw.dev/recent.json")
 		.then((r) => r.json())
@@ -6,35 +10,82 @@
 </script>
 
 <footer>
-	<p class="copyright">(C) 2022 KekOnTheWorld</p>
-	{#if events}
-		<div class="events">
-			{#each events as event}
-				<div class="event">
-					<a href={event.link} target="_blank">{event.name}</a>
-				</div>
-			{/each}
-		</div>
-	{/if}
+    <div class="wrapper">
+        <div class="big">
+            <p class="emoji">👋</p>
+            <p class="short">This awesome opensource account system was made by KekOnTheWorld. Check it out on <a href="https://github.com/KotwOSS/kekaccount" target="_blank">Github</a>!</p>
+        </div>
+        <div class="categories">
+            <div class="legal category">
+                <h4>Legal</h4>
+                {#if privacy}<a href={privacy}><T k={lk.FOOTER_PRIVACY} /></a>{/if}
+                {#if imprint}<a href={imprint}><T k={lk.FOOTER_IMPRINT} /></a>{/if}
+            </div>
+            
+            {#if events}
+            <div class="events category">
+                <h4>Events</h4>
+                {#each events as event}
+                <div class="event">
+                    <a href={event.link} target="_blank">{event.name}</a>
+                </div>
+                {/each}
+            </div>
+            {/if}
+        </div>
+    </div>
+    <p class="copyright">(C) 2022 KekOnTheWorld</p> 
 </footer>
 
 <style>
-	footer {
+    .big>.emoji {
+        font-size: 70px;
+        margin-bottom: 5px;
+    }
+
+    .big {
+        padding: 30px;
+        margin-bottom: 20px;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        /* border: 1px solid var(--card-border); */
+    }
+
+    footer {
+        padding: 10px 0;
+    }
+
+	.wrapper {
 		width: 100%;
 		padding: 15px;
 		display: flex;
-		flex-direction: column;
+		flex-direction: row;
+        flex-wrap: wrap;
 		gap: 20px;
-		justify-content: center;
-		align-items: center;
+		justify-content: space-around;
+        margin-bottom: 30px;
+		/* align-items: center; */
 	}
 
-	.events {
-		display: flex;
-		flex-direction: row;
-		justify-content: center;
-		align-items: center;
-		flex-wrap: wrap;
-		gap: 15px;
-	}
+    .wrapper>.categories {
+        display: flex;
+        flex-direction: row;
+        flex-wrap: wrap;
+        gap: 20px;
+        column-gap: 100px;
+        align-items: center;
+        padding: 0 15px;
+    }
+
+    .category * {
+        text-align: left;
+    }
+
+    .category {
+        display: flex;
+        flex-direction: column;
+        gap: 5px;
+    }
 </style>
--- frontend/src/lib/lang.ts
+++ frontend/src/lib/lang.ts
@@ -43,6 +43,9 @@ export enum LangKey {
 	NAV_HOME = "nav.home",
 	NAV_ACCOUNT = "nav.account",
 	NAV_LOGOUT = "nav.logout",
+    // FOOTER
+    FOOTER_PRIVACY = "footer.privacy",
+    FOOTER_IMPRINT = "footer.imprint",
 	// REGISTER
 	REGISTER_KEKY_NEXT = "register.keky.next",
 	REGISTER_KEKY_BACK = "register.keky.back",
