256cfbc5ce69ebb69a9794961f00ef18997b648e
KekOnTheWorld <kekontheworld@gmail.com>
2022-04-02 15:14:12 +0200

    Added dashboard index page

--- frontend/src/routes/dash/index.svelte
+++ frontend/src/routes/dash/index.svelte
@@ -1,24 +1,57 @@
 <script>
 	import { client } from "$lib/client";
-	import { LangKey as lk } from "$lib/lang";
+	import { LangKey as lk, language } from "$lib/lang";
 	import T from "$components/translate.svelte";
 
 	let user = client.user;
 
 	console.log($user);
+
+	const categories = [
+		{
+			title: language[lk.ACCOUNT_TITLE],
+			description: language[lk.ACCOUNT_DESCRIPTION],
+			link: "/dash/account"
+		},
+		{
+			title: language[lk.APPS_TITLE],
+			description: language[lk.APPS_DESCRIPTION],
+			link: "/dash/apps"
+		},
+		{
+			title: language[lk.TOKENS_TITLE],
+			description: language[lk.TOKENS_DESCRIPTION],
+			link: "/dash/tokens"
+		}
+	];
 </script>
 
 <div class="root fadein">
-	<h1><T k={lk.DASHBOARD_TITLE} /></h1>
+	<h1 class="title"><T k={lk.DASHBOARD_TITLE} /></h1>
 	{#if !$user.verified}
 		<p class="hint short"><T k={lk.HINT_VERIFY} /></p>
 	{/if}
-	<p>{$user.name}</p>
-	<p class="break">{$user.id}</p>
-	<p>{$user.email}</p>
+
+	<div class="categories">
+		{#each categories as category}
+			<a class="category card" href={category.link}>
+				<h2>{category.title}</h2>
+				<p>{category.description}</p>
+			</a>
+		{/each}
+	</div>
 </div>
 
 <style>
+	.categories {
+		margin-top: 20px;
+		display: flex;
+		flex-direction: column;
+		gap: 15px;
+		justify-content: center;
+		align-items: center;
+	}
+
 	.root {
 		display: flex;
 		flex-direction: column;
