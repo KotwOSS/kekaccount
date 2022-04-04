9fb6ea9874524037d40fd6869bfcfe18604f080b
KekOnTheWorld <kekontheworld@gmail.com>
2022-04-04 20:57:19 +0200

    Initialized app create page

---
+++ frontend/src/routes/dash/apps/create.svelte
@@ -0,0 +1,15 @@
+<div class="root fadein"></div>
+
+<style>
+	.root > :global(.loader) {
+		width: 50px;
+		height: 50px;
+	}
+
+	.root {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+	}
+</style>
