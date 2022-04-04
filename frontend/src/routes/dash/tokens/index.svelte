fdd116d5477f0b4e9a40146ec862c95938b553e9
KekOnTheWorld <kekontheworld@gmail.com>
2022-04-04 20:57:00 +0200

    Switched to TokensTable component

--- frontend/src/routes/dash/tokens/index.svelte
+++ frontend/src/routes/dash/tokens/index.svelte
@@ -1,11 +1,12 @@
 <script lang="ts">
 	import { client } from "$lib/client";
-	import { LangKey as lk, language } from "$lib/lang";
-	import T from "$components/translate.svelte";
+	import { LangKey as lk, language as ln } from "$lib/lang";
 	import { APIError, Routes } from "$lib/api";
+
 	import Loader from "$components/loader.svelte";
-	import Error from "$components/error.svelte";
-	import { goto } from "$app/navigation";
+    import TokensTable from "$components/table/tokens.svelte";
+	
+    import { goto } from "$app/navigation";
 
 	let user = client.user;
 
@@ -17,7 +18,7 @@
 		.then((r) => (tokens = r))
 		.catch((e) => {
 			if (e instanceof APIError) error = e.get_message();
-			else error = language[lk.ERROR_CONNECTION];
+			else error = ln[lk.ERROR_CONNECTION];
 		});
 
 	function create() {
@@ -26,26 +27,20 @@
 </script>
 
 <div class="root fadein">
-	<h1 class="title"><T k={lk.TOKENS_TITLE} /></h1>
+	<h1 class="title">{ln[lk.TOKENS_TITLE]}</h1>
 	{#if !$user.verified}
-		<p class="hint short"><T k={lk.HINT_VERIFY} /></p>
+		<p class="hint short">{ln[lk.HINT_VERIFY]}</p>
 	{/if}
-	<p class="description"><T k={lk.TOKENS_DESCRIPTION} /> <T k={lk.BACK_TO_DASHBOARD} /></p>
-	<Error {error} />
+	<p class="description">{ln[lk.TOKENS_DESCRIPTION]} {@html ln[lk.BACK_TO_DASHBOARD]}</p>
+	{#if error}<p class="error break short">{error}</p>{/if}
 
-	{#if tokens}
-		<div class="tokens">
-			{#each tokens as token}
-				<div class="token card">
-					<p class="token-name">{token.name}</p>
-					<p class="token-id">{token.id}</p>
-				</div>
-			{/each}
-		</div>
-	{:else}
-		<Loader />
-	{/if}
-	<button on:click={create}><T k={lk.TOKENS_CREATE} /></button>
+    {#if tokens}
+        <TokensTable data={tokens} />
+    {:else}
+        <Loader />
+    {/if}
+
+	<button class="create" on:click={create}>{ln[lk.TOKENS_CREATE]}</button>
 </div>
 
 <style>
@@ -54,15 +49,9 @@
 		height: 50px;
 	}
 
-	.tokens {
-		max-width: 500px;
-		display: flex;
-		flex-wrap: wrap;
-		justify-content: center;
-		align-items: center;
-		gap: 10px;
-		margin-bottom: 20px;
-	}
+    .create {
+        margin-top: 15px;
+    }
 
 	.root {
 		display: flex;
