809d4ae4a6dc02d6e60e84d53c96c475582bd096
KekOnTheWorld <kekontheworld@gmail.com>
2022-04-09 21:01:42 +0200

    Set verified to true when authorized

--- frontend/src/routes/verify.svelte
+++ frontend/src/routes/verify.svelte
@@ -1,25 +1,31 @@
 <script>
-    import { goto } from "$app/navigation";
-import { Routes } from "$lib/api";
-    import { client } from "$lib/client";
-    import { LangKey as lk, language } from "$lib/lang";
+	import { goto } from "$app/navigation";
+	import { Routes } from "$lib/api";
+	import { client } from "$lib/client";
+	import { LangKey as lk, language } from "$lib/lang";
 
+	let params = new URLSearchParams(window.location.search);
+	let id = params.get("id");
 
-    let params = new URLSearchParams(window.location.search);
-    let id = params.get("id");
+	if (id) {
+		client.confirm_access({
+			callback: async function (identifier) {
+				await Routes.Auth.VERIFY.send({
+					identifier,
+					id
+				});
 
-    if(id) {
-        client.confirm_access({
-            callback: async function(identifier) {
-                await Routes.Auth.VERIFY.send({
-                    identifier,
-                    id
-                });
-                goto("/");
-            },
-            description: {
-                hint: language[lk.VERIFY_CONFIRM_HINT]
-            }
-        });
-    } else goto("/");
+				if (client.authorized)
+					client.user.update(function (user) {
+						user.verified = true;
+						return user;
+					});
+
+				goto("/");
+			},
+			description: {
+				hint: language[lk.VERIFY_CONFIRM_HINT]
+			}
+		});
+	} else goto("/");
 </script>
