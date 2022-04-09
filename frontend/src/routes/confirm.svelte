ef525f747e069bf665797e2a2ad96e60d8d0bd40
KekOnTheWorld <kekontheworld@gmail.com>
2022-04-09 20:59:31 +0200

    Format

--- frontend/src/routes/confirm.svelte
+++ frontend/src/routes/confirm.svelte
@@ -11,18 +11,18 @@
 	let loading: boolean = false;
 	let error: string;
 
-    let user = client.user;
-    let authorized = client.authorized;
+	let user = client.user;
+	let authorized = client.authorized;
 
-    let confirm: Confirm = client.confirm;
-    console.log(confirm);
+	let confirm: Confirm = client.confirm;
+	console.log(confirm);
 
-    let type_to_confirm: string;
+	let type_to_confirm: string;
 
-    let disabled: boolean = false;
-    $: disabled = confirm && confirm.type_to_confirm && confirm.type_to_confirm !== type_to_confirm;
+	let disabled: boolean = false;
+	$: disabled = confirm && confirm.type_to_confirm && confirm.type_to_confirm !== type_to_confirm;
 
-	let uoe: string = $user?$user.name:null;
+	let uoe: string = $user ? $user.name : null;
 	let password: string;
 
 	async function submit(e) {
@@ -36,64 +36,73 @@
 			? { email: uoe, password: hashed_password }
 			: { username: uoe, password: hashed_password };
 
-        try {
-            await confirm.callback(identifier);
-        } catch(e) {
-            if (e instanceof APIError) {
-                if (e.status === 401) error = ln[lk.ERROR_CREDENTIALS];
-                else error = e.get_message();
-            } else if(e instanceof TypeError) error = ln[lk.ERROR_CONNECTION];
-            else error = e.message;
-            loading = false;
-        }
+		try {
+			await confirm.callback(identifier);
+		} catch (e) {
+			if (e instanceof APIError) {
+				if (e.status === 401) error = ln[lk.ERROR_CREDENTIALS];
+				else error = e.get_message();
+			} else if (e instanceof TypeError) error = ln[lk.ERROR_CONNECTION];
+			else error = e.message;
+			loading = false;
+		}
 	}
 
-    if(!confirm) goto("/dash");
+	if (!confirm) goto("/dash");
 </script>
 
 <div class="root fadein">
 	<form class="card" on:submit={submit}>
-        {#if confirm}
-		<h1 class="title">{ln[lk.CONFIRM_TITLE]}</h1>
-		<p class="description">{ln[lk.CONFIRM_DESCRIPTION]}</p>
-		{#if error}<p class="error break short">{error}</p>{/if}
-		<input disabled={$authorized} bind:value={uoe} type="text" id="uoe" placeholder={ln[lk.LOGIN_UOE]} />
-		<input
-			bind:value={password}
-			type="password"
-			id="password"
-			placeholder={ln[lk.LOGIN_PASSWORD]}
-		/>
+		{#if confirm}
+			<h1 class="title">{ln[lk.CONFIRM_TITLE]}</h1>
+			<p class="description">{ln[lk.CONFIRM_DESCRIPTION]}</p>
+			{#if error}<p class="error break short">{error}</p>{/if}
+			<input
+				disabled={$authorized}
+				bind:value={uoe}
+				type="text"
+				id="uoe"
+				placeholder={ln[lk.LOGIN_UOE]}
+			/>
+			<input
+				bind:value={password}
+				type="password"
+				id="password"
+				placeholder={ln[lk.LOGIN_PASSWORD]}
+			/>
 
-        {#if confirm.description.hint}
-            <p class="hint short">{confirm.description.hint}</p>
-        {:else if confirm.description.warning}
-            <p class="warning short">{confirm.description.warning}</p>
-        {/if}
+			{#if confirm.description.hint}
+				<p class="hint short">{confirm.description.hint}</p>
+			{:else if confirm.description.warning}
+				<p class="warning short">{confirm.description.warning}</p>
+			{/if}
 
-        {#if confirm.type_to_confirm}
-            <div class="type_to_confirm">
-                <p class="short"><span class="hint">{ln[lk.CONFIRM_TYPE_TO_CONFIRM]}</span> {confirm.type_to_confirm}</p>
-                <input bind:value={type_to_confirm} type="text">
-            </div>
-        {/if}
+			{#if confirm.type_to_confirm}
+				<div class="type_to_confirm">
+					<p class="short">
+						<span class="hint">{ln[lk.CONFIRM_TYPE_TO_CONFIRM]}</span>
+						{confirm.type_to_confirm}
+					</p>
+					<input bind:value={type_to_confirm} type="text" />
+				</div>
+			{/if}
 
-        <div class="spacer"></div>
+			<div class="spacer" />
 
-		<LoadingButton bind:loading bind:disabled>{ln[lk.CONFIRM_SUBMIT]}</LoadingButton>
-        {/if}
+			<LoadingButton bind:loading bind:disabled>{ln[lk.CONFIRM_SUBMIT]}</LoadingButton>
+		{/if}
 	</form>
 </div>
 
 <style>
-    .type_to_confirm {
-        margin-top: 10px;
-        margin-bottom: 10px;
-    }
+	.type_to_confirm {
+		margin-top: 10px;
+		margin-bottom: 10px;
+	}
 
-    .type_to_confirm>p {
-        margin-bottom: 10px;
-    }
+	.type_to_confirm > p {
+		margin-bottom: 10px;
+	}
 
 	.card {
 		display: flex;
@@ -119,9 +128,9 @@
 		margin-bottom: 20px;
 	}
 
-    .spacer {
-        margin-bottom: 10px;
-    }
+	.spacer {
+		margin-bottom: 10px;
+	}
 
 	.root {
 		display: flex;
