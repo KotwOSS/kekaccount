61c2813b57b00057766cbb851ce420f6d1bc91bf
KekOnTheWorld <kekontheworld@gmail.com>
2022-04-22 15:32:21 +0200

    Redesigned dashboard

--- frontend/src/routes/dash/tokens/create.svelte
+++ frontend/src/routes/dash/tokens/create.svelte
@@ -4,7 +4,7 @@
 	import { client } from "$lib/client";
 	import { Tokens } from "$lib/store";
 	import { LangKey as lk, language as ln } from "$lib/lang";
-    import PermissionSelect from "$components/table/permission/selector.svelte";
+	import PermissionSelect from "$components/table/permission/selector.svelte";
 
 	let name: string;
 	let permissions: number = 0;
@@ -37,30 +37,30 @@
 	}
 </script>
 
-<form class="root fadein" on:submit={create}>
+<form class="inner" on:submit={create}>
 	<h1 class="title">{ln[lk.TOKENS_CREATE]}</h1>
 	<p class="description">{@html ln[lk.TOKENS_CREATE_DESCRIPTION]}</p>
 	<input class="name" type="text" bind:value={name} placeholder={ln[lk.TOKENS_CREATE_NAME]} />
-	<PermissionSelect bind:permissions={permissions} />
+	<PermissionSelect bind:permissions />
 	<button class="create">{ln[lk.TOKENS_CREATE_SUBMIT]}</button>
 </form>
 
 <style>
-	.root > :global(.loader) {
+	.inner > :global(.loader) {
 		width: 50px;
 		height: 50px;
 	}
 
-	.root {
+	.inner {
 		display: flex;
 		flex-direction: column;
 		justify-content: center;
 		align-items: center;
 	}
 
-    .name {
-        margin-bottom: 10px;
-    }
+	.name {
+		margin-bottom: 10px;
+	}
 
 	.description {
 		margin-bottom: 10px;
