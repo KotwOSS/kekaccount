020d63e6f9c2b00fb29bc7d52906dc39d845234c
KekOnTheWorld <kekontheworld@gmail.com>
2022-04-22 15:38:38 +0200

    Redesigned navbar

--- frontend/src/components/navbar.svelte
+++ frontend/src/components/navbar.svelte
@@ -3,80 +3,127 @@
 
 	import { LangKey as lk, language as ln } from "$lib/lang";
 	import { client } from "$lib/client";
+	import Link from "$components/link.svelte";
+	import Category from "./category.svelte";
 
 	let path = window.location.pathname;
 
-	afterNavigate(function (navigation) {
-		path = navigation.to.pathname;
+	afterNavigate(function ({ to }) {
+		path = to.pathname;
 	});
 
 	let expand_menu: boolean = false;
 
 	let authorized = client.authorized;
 
-    let html = document.querySelector("html");
-
-    $: html.setAttribute("style", expand_menu?"overflow-y: hidden":"");
+	let html = document.documentElement;
+	$: html.setAttribute("style", expand_menu ? "overflow-y: hidden" : "");
 </script>
 
-<div on:click={function() {
-    expand_menu = false;
-}} class="darken" class:expand={expand_menu}></div>
+<div
+	on:click={function () {
+		expand_menu = false;
+	}}
+	class="darken"
+	class:expand={expand_menu}
+/>
 
-<div class="spacer"></div>
+<div class="spacer" />
 
 <div class="menu" class:expand={expand_menu}>
-	<a href="/" class:active={path === "/"}>🏠 {ln[lk.NAV_HOME]}</a>
+	<Link href="/" exact>
+		🏠 {ln[lk.NAV_HOME]}
+	</Link>
+
 	{#if $authorized}
-		<a href="/dash" class:active={path.startsWith("/dash")}>✏️ {ln[lk.NAV_DASHBOARD]}</a>
-		<a href="/logout" class:active={path.startsWith("/logout")}>⚔️ {ln[lk.NAV_LOGOUT]}</a>
+		<Category>
+			<Link href="/dash" slot="title">
+				✏️ {ln[lk.NAV_DASHBOARD]}
+			</Link>
+			<svelte:fragment slot="sub">
+				<Link href="/dash/account/">Account</Link>
+				<Link href="/dash/apps/">Apps</Link>
+				<Link href="/dash/tokens/">Tokens</Link>
+			</svelte:fragment>
+		</Category>
+
+		<Link href="/logout">
+			⚔️ {ln[lk.NAV_LOGOUT]}
+		</Link>
 	{:else}
-		<a href="/register" class:active={path.startsWith("/register")}>{ln[lk.NAV_REGISTER]}</a>
-		<a href="/login" class:active={path.startsWith("/login")}>{ln[lk.NAV_LOGIN]}</a>
+		<div class="link">
+			<a href="/register" class:active={path.startsWith("/register")}>{ln[lk.NAV_REGISTER]}</a>
+		</div>
+		<div class="link">
+			<a href="/login" class:active={path.startsWith("/login")}>{ln[lk.NAV_LOGIN]}</a>
+		</div>
 	{/if}
 </div>
 
 <nav>
-	<div class="burger" class:expand={expand_menu} on:click={function () {
-        expand_menu = !expand_menu;
-    }}>
-		<div />
-		<div />
-		<div />
-	</div>
+	<div class="wrapper">
+		<div
+			class="burger"
+			class:expand={expand_menu}
+			on:click={function () {
+				expand_menu = !expand_menu;
+			}}
+		>
+			<div />
+			<div />
+			<div />
+		</div>
 
-	<h1>{ln[lk.NAV_TITLE]}</h1>
-	<div class="links">
-		<a href="/" class:active={path === "/"}>{ln[lk.NAV_HOME]}</a>
-		{#if $authorized}
-			<a href="/dash" class:active={path.startsWith("/dash")}>{ln[lk.NAV_DASHBOARD]}</a>
-			<a href="/logout" class:active={path.startsWith("/logout")}>{ln[lk.NAV_LOGOUT]}</a>
-		{:else}
-			<a href="/register" class:active={path.startsWith("/register")}>{ln[lk.NAV_REGISTER]}</a>
-			<a href="/login" class:active={path.startsWith("/login")}>{ln[lk.NAV_LOGIN]}</a>
-		{/if}
+		<h1>{ln[lk.NAV_TITLE]}</h1>
+		<div class="links">
+			<Link href="/" exact>
+				🏠 {ln[lk.NAV_HOME]}
+			</Link>
+			{#if $authorized}
+				<Link href="/dash">
+					✏️ {ln[lk.NAV_DASHBOARD]}
+				</Link>
+				<Link href="/logout">
+					⚔️ {ln[lk.NAV_LOGOUT]}
+				</Link>
+			{:else}
+				<div class="link">
+					<a href="/register" class:active={path.startsWith("/register")}>{ln[lk.NAV_REGISTER]}</a>
+				</div>
+				<div class="link">
+					<a href="/login" class:active={path.startsWith("/login")}>{ln[lk.NAV_LOGIN]}</a>
+				</div>
+			{/if}
+		</div>
 	</div>
 </nav>
 
 <style>
-	nav > h1 {
+	nav > .wrapper > h1 {
 		font-size: 1.6em;
-		letter-spacing: 1px;
 	}
 
 	nav {
 		display: flex;
-		align-items: center;
-		padding: 0 15px;
-		height: 63px;
+		justify-content: center;
+		height: var(--nav-height);
 		width: 100vw;
 		z-index: 2;
-        top: 0;
+		top: 0;
 		left: 0;
 		position: fixed;
 	}
 
-	nav > .links {
+	nav > .wrapper {
+		display: flex;
+		align-items: center;
+		padding: 0 15px;
+		height: 100%;
+		width: 100%;
+		max-width: 1300px;
+	}
+
+	nav > .wrapper > .links {
 		font-size: 18px;
 		display: flex;
 		justify-content: flex-end;
@@ -85,63 +132,56 @@
 		gap: 20px;
 	}
 
-	nav > .links > a {
-		text-decoration: none;
-	}
-
-	nav > .burger {
+	nav > .wrapper > .burger {
 		display: none;
 	}
 
-    .spacer {
-        height: 63px;
-    }
+	.spacer {
+		height: var(--nav-height);
+	}
 
 	.menu {
-        align-items: flex-start;
-        position: fixed;
+		align-items: flex-start;
+		position: fixed;
 		font-size: 20px;
 		display: flex;
 		flex-direction: column;
-        row-gap: 20px;
+		row-gap: 20px;
 		transform: translateX(-100%);
-        padding-top: 20px;
-        padding-left: 20px;
-		/* position: absolute; */
+		padding-top: 20px;
+		padding-left: 20px;
 		top: 63px;
 		left: 0;
 		width: 100%;
 		max-width: 260px;
-		height: calc(100vh - 50px);
+		height: calc(100vh - var(--nav-height));
 		background-color: var(--nav-background);
 		z-index: 1;
 		padding-bottom: 63px;
-        transition: transform 0.3s ease;
+		transition: transform 0.3s ease;
 	}
 
-    .darken {
-        position: fixed;
-        z-index: 1;
-        /* background-color: rgba(0, 0, 0, 0.7);
-        background: rgba(0, 0, 0, 0.7) none repeat scroll 0% 0%; */
-        width: 100vw;
-        height: 100vh;
-        opacity: 0;
-        pointer-events: none;
-        transition: opacity 0.3s ease;
-    }
+	.darken {
+		position: fixed;
+		z-index: 1;
+		width: 100vw;
+		height: 100vh;
+		opacity: 0;
+		pointer-events: none;
+		transition: opacity 0.3s ease;
+	}
 
-    .darken.expand {
-        opacity: 1;
-        pointer-events: all;
-    }
+	.darken.expand {
+		opacity: 1;
+		pointer-events: all;
+	}
 
 	.menu.expand {
 		transform: translateX(0);
 	}
 
 	@media (max-width: 600px) {
-		nav > .burger {
+		nav > .wrapper > .burger {
 			display: flex;
 			flex-direction: column;
 			justify-content: space-between;
@@ -151,25 +191,25 @@
 			margin-right: 25px;
 		}
 
-		nav > .burger > div {
+		nav > .wrapper > .burger > div {
 			width: 100%;
 			height: 1.6px;
-            transition: all 0.5s ease;
+			transition: all 0.5s ease;
 		}
 
-        nav>.burger.expand>div:nth-child(1) {
-            transform: translate(0, 9px) rotate(40deg);
-        }
+		nav > .wrapper > .burger.expand > div:nth-child(1) {
+			transform: translate(0, 9px) rotate(40deg);
+		}
 
-        nav>.burger.expand>div:nth-child(2) {
-            opacity: 0;
-        }
+		nav > .wrapper > .burger.expand > div:nth-child(2) {
+			opacity: 0;
+		}
 
-        nav>.burger.expand>div:nth-child(3) {
-            transform: translate(0, -9px) rotate(-40deg);
-        }
+		nav > .wrapper > .burger.expand > div:nth-child(3) {
+			transform: translate(0, -9px) rotate(-40deg);
+		}
 
-		nav > .links {
+		nav > .wrapper > .links {
 			display: none;
 		}
 	}
